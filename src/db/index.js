82a69442f630f525e40e51411590260dad591ee3
codeWith-deep <sajan8288786@gmail.com>
2024-06-02 14:16:00 +0400

    connect to mongodb atlas

---
+++ src/db/index.js
@@ -0,0 +1,28 @@
+
+import mongoose from "mongoose";
+import  {DBNAME} from "../constants.js";
+
+
+
+const connectDB = async  () => {
+
+    try {
+        const connectionInstance = await mongoose.connect('mongodb+srv://newuser:gurseweksir123@cluster0.ijnrcf0.mongodb.net/adb');
+            // console.log(MONGODB_URI);
+            // console.log(` MongoDB Connected !! DB HOST : ${connectionInstance.connection.host}`);
+            console.log("mongdb connected successfully")
+
+         
+    } catch (error) {
+        console.log("MONGODB Connection FAILED", error);
+        // console.log(error, "not working")
+        process.exit(1)
+    }
+}
+
+connectDB()
+
+export    {connectDB};
+
+
+
