f8e17330cd8dbb50198bf1448d20add49850ef3b
codeWith-deep <sajan8288786@gmail.com>
2024-06-07 17:59:11 +0400

    all setup configurade

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// import router
+import userRouter from './routes/user.routes.js'
+
+//Router declaration 
+app.use("/api/v1/users", userRouter);
+
+
 export  {app}
 // console.log(process.env.CORS_ORIGIN)
 
